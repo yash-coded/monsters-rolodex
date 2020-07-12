a89365971e0f78ad8067f913a3f776741edf6cb2
Yash Patel <yash@Yashs-MBP.cg.shawcable.net>
2020-07-12 12:53:58 -0600

    Updates

---
+++ precache-manifest.cd768e97536ad10975d01ca03789ab8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca43df5626d9d24ed9632646798d85e2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "67d8d307704df5c7c33b",
+    "url": "/monsters-rolodex/static/css/main.eb2a0d0a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d8d307704df5c7c33b",
+    "url": "/monsters-rolodex/static/js/main.7b40a421.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
